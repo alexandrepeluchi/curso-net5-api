cdaf533fd0d03ee24b352670569538fed95d29c4
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2021-07-26 20:29:05 -0300

    Adicionado os métodos na interface Movie

--- WebApi/Services/Movies/IMovieService.cs
+++ WebApi/Services/Movies/IMovieService.cs
@@ -1,11 +1,16 @@
 using System.Threading;
 using System.Threading.Tasks;
 using WebApi.DTOs.Movies.GET;
+using WebApi.Models;
 
 namespace WebApi.Services.Movies
 {
     public interface IMovieService
     {
+        Task<Movie> Create(Movie movie);
+        Task Delete(long id);
+        Task<Movie> GetById(long id);
         Task<MoviesListOutputGetAllDTO> GetByPageAsync(int limit, int page, CancellationToken cancellationToken);
+        Task<Movie> Update(Movie movie, long id);
     }
 }
